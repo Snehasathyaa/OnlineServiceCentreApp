bc93bfd33ab0740fdf5b0b28170b953045d0a679
snehakp7 <110010801+snehakp7@users.noreply.github.com>
2022-10-13 15:49:56 +0530

    nxtpage

--- lib/nextpage.dart
+++ lib/nextpage.dart
@@ -1,12 +1,11 @@
-import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
-class textpage extends StatelessWidget {
-  const textpage({super.key});
+class nxtpage extends StatelessWidget {
+  const nxtpage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Container();
   }
 }
